a5a5476a45b3303d67872c02274f29454756c6d3
10dndnskxkd <megunoshira@gmail.com>
2025-01-16 10:44:21 +0800

    Terminal class without functions

---
+++ PRG2-Assignment/Terminal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    class Terminal
+    {
+        private string terminalName;
+        public string TerminalName
+        {
+            get { return terminalName; }
+            set { terminalName = value; }
+        }
+
+        public Dictionary<string, Airline> airlines
+        {
+            get; set;
+        }
+
+        public Dictionary<string, Flight> flights
+        {
+            get; set;
+        }
+
+        public Dictionary<string, BoardingGate> boardingGates
+        {
+            get; set;
+        }
+
+        public Dictionary<string, double> gateFees
+        {
+            get; set;
+        }
+
+    }
+}
